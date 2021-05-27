4c3757ab2d72e8df69e0138eb576501dbce1fe5e
Adam Lawson <adamjlawson22@gmail.com>
2021-05-27 07:33:59 +0100

    sdm710-common: QTI Bluetooth

--- sdm710.mk
+++ sdm710.mk
@@ -131,7 +131,10 @@ PRODUCT_PACKAGES += \
     BluetoothResCommon \
     libbthost_if \
     libldacBT_dec \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor
+    libbluetooth_qti \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
