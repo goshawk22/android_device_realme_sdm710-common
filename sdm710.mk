9e96d5f783223b8ffb48c9339395175f5c20f072
Adam Lawson <adamjlawson22@gmail.com>
2021-08-15 17:48:04 +0100

    sdm710-common: Uprev to soundtrigger 2.3

--- sdm710.mk
+++ sdm710.mk
@@ -73,7 +73,7 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
-    android.hardware.soundtrigger@2.2-impl:32 \
+    android.hardware.soundtrigger@2.3-impl:32 \
     android.hardware.audio.effect@2.0-service \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
